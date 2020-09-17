e994456d5aff9d1f98f0ffcf7854c1e86a3bc48e
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2020-09-17 12:49:47 +0200

    Add files via upload

---
+++ SocketTeknologi/Client2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+        static void ExecuteClient()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[1];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            sender.Connect(localEndPoint);
+            Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+            Console.WriteLine("skriv exit anytime du vil quitte chatten");
+            Console.WriteLine("Skriv dine beskeder her: ");
+            bool stillActive = true;
+            while (stillActive)
+            {
+                string result = Console.ReadLine();
+                if (result.ToLower() != "exit")
+                {
+                    byte[] message = Encoding.ASCII.GetBytes(result);
+                    sender.Send(message);
+                }
+                else
+                {
+                    stillActive = false;
+                    byte[] message = Encoding.ASCII.GetBytes(result);
+                    sender.Send(message);
+                }
+
+            }
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
---
+++ SocketTeknologi/ServerSide/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+        public static void ExecuteServer()
+        {
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[1];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            Console.WriteLine("Waiting connection ... ");
+            Console.WriteLine("Got a connection");
+            Socket clientSocket;
+            bool serverActive = true;
+            while (serverActive)
+            {
+                Thread t = new Thread(() => HandleConnection(clientSocket = listener.Accept()));
+                t.Start();
+            }
+
+            //HandleConnection(clientSocket);
+            /*
+            string result = "";
+            do
+            {
+                byte[] messageRecieved = new byte[1024];
+                int byteNumber = clientSocket.Receive(messageRecieved);
+                result = Encoding.ASCII.GetString(messageRecieved, 0, byteNumber);
+                if (result != "exit")
+                    Console.WriteLine(result);
+            } while (result != "exit");
+            Console.WriteLine("Connection terminated, shutting down");
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            */
+        }
+        public static void HandleConnection(Socket s)
+        {
+            string result = "";
+            do
+            {
+                byte[] messageRecieved = new byte[1024];
+                int byteNumber = s.Receive(messageRecieved);
+                result = Encoding.ASCII.GetString(messageRecieved, 0, byteNumber);
+                if (result != "exit")
+                    Console.WriteLine(result);
+            } while (result != "exit");
+            Console.WriteLine("Connection terminated with {0}, shutting down", s.ToString());
+            s.Shutdown(SocketShutdown.Both);
+            s.Close();
+        }
+    }
+}
