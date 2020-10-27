6ab913daba37b75795b5af7af96ef6f10f846133
rasmusmedj <rasmusfranorden@hotmail.com>
2020-10-27 11:43:09 +0100

    Ik en skid

---
+++ MatStacks/MatStacks/Controllers/ExerciseSubjectController.cs
@@ -0,0 +1,25 @@
+﻿using MatStacks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatStacks.Controllers
+{
+    public class ExerciseSubjectController : Controller
+    {
+        private readonly ExerciseSubjectDataContext db;
+        public ExerciseSubjectController(ExerciseSubjectDataContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index(long? id)
+        {
+            var exerciseSubject = db.ExerciseSubjects.Where(x => x.Id == id)
+                .Include(x => x.Exercises).FirstOrDefault();
+            return View(exerciseSubject);
+        }
+    }
+}
---
+++ MatStacks/MatStacks/Models/Exercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatStacks.Models
+{
+    public class Exercise
+    {
+        public string Title { get; set; }
+        public long Id { get; set; }
+        public string Body { get; set; }
+        public User Author { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int Rating { get; set; }
+        public string Difficulty { get; set; }
+        public List<Answer> Answers { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
