8a8f05360a7ab2b7a3a8cbffb109007f620e95d8
rasmusmedj <rasmusfranorden@hotmail.com>
2020-12-08 10:35:08 +0100

    API tilføjet til master
    JA hvad tror du selv???? <3

---
+++ MatStacksAPI/MatStacksAPI/Controllers/ExerciseController.cs
@@ -0,0 +1,58 @@
+﻿using MatStacksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace MatStacksAPI.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class ExerciseController : ControllerBase
+    {
+        //:))))))
+        private ExerciseSubjectDataContext db;
+        public ExerciseController(ExerciseSubjectDataContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult GetAllExercises()
+        {
+            return Ok(db.Exercises.ToList());
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(long id)
+        {
+            var result = db.Exercises.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateExercise(Exercise exercise)
+        {
+            db.Exercises.Add(exercise);
+            db.SaveChanges();
+            return CreatedAtAction("GetById", new { id = exercise.Id }, exercise);
+        }
+
+        [HttpDelete("Delete/{Id}")]
+        public IActionResult DeleteExercise(long id)
+        {
+            var result = db.Exercises.Find(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            db.Exercises.Remove(result);
+            db.SaveChanges();
+            return Ok(result);
+        }
+    }
+}
---
+++ MatStacksAPI/MatStacksAPI/Models/ExerciseSubjectDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatStacksAPI.Models
+{
+    public class ExerciseSubjectDataContext : DbContext
+    {
+        public DbSet<ExerciseSubject> ExerciseSubjects { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+
+        public ExerciseSubjectDataContext(DbContextOptions<ExerciseSubjectDataContext> options) : base(options)
+        {
+            //Tjekker om der findes en database, gør der ikke det - så danner den en ny database 
+            Database.EnsureCreated();
+        }
+    }
+}
