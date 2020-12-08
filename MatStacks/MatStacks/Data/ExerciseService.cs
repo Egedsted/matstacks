ba19f0c4da07888563c820444ef5e91bbc88b04b
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2020-12-08 11:46:45 +0100

    Blazor projekt
    Blazor

---
+++ MatStacks/MatStacks/Data/ExerciseService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Matstacks.Data
+{
+    public class ExerciseService
+    {
+        private string connectionString = "http://localhost:49519/";
+        private HttpClient ApiClient;
+        public ExerciseService()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Exercise>> GetExercises()
+        {
+            using(HttpResponseMessage response = await ApiClient.GetAsync(connectionString + "exercise"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Exercise>>(jsonString);
+                }
+                return null;
+            }
+        }
+    }
+}
