5e8ccfccd853b4e5e2a54c4a1d015f0ece67bcf3
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2020-12-14 11:45:22 +0100

    Exercisecontroller & views implementeret
    OmegaLUL

---
+++ MatStacks/MatStacks/Controllers/ExerciseController.cs
@@ -0,0 +1,40 @@
+﻿using MatStacks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatStacks.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        private readonly ExerciseSubjectDataContext db;
+        public ExerciseController(ExerciseSubjectDataContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index(long id)
+        {
+            return View(db.Exercises.Find(id));
+        }
+
+        public IActionResult Create(long id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Exercise exercise, long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            exercise.Author = User.Identity.Name;
+            exercise.CreateDate = DateTime.Now;
+            db.Exercises.Add(exercise);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", db.Exercises.Find(exercise).Id);
+        }
+    }
+}
